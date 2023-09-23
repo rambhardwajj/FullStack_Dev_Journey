c6f40468ff62b94a15ab279d59388aba1b39833f
rambhardwajj <rambhardwaj101000@gmail.com>
2023-09-23 16:45:55 +0530

    TS - interfaces, types, enums

---
+++ Week7/TS/sum.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function calc(a, b) {
+    return a + b;
+}
+var ans = calc(1, 2);
+console.log(ans);
---
+++ Week7/TS/sum.ts
@@ -0,0 +1,43 @@
+interface Circle {
+	radius : number
+}
+interface Square {
+	side : number
+}
+interface Rectangle{
+	width: number,
+	height: number 
+}
+
+enum Arthematic {
+	Add,
+	Sub,
+	Div,
+	Mul
+}
+
+type ShapeOR = Rectangle | Circle | Square;
+type ShapeUnion = Rectangle | Circle| Square;
+
+function renderShape( shape : ShapeOR ){
+	console.log(shape);
+}
+function calculateArea ( shape :ShapeUnion ){
+	console.log("calc Area");
+}
+
+renderShape(
+	{
+		radius: 10,
+		side: 4,
+		width: 3,
+		height:1
+	}
+)
+
+function calculator( a: number, b:number , type : Arthematic)  {
+	console.log(type);
+}
+
+const ans = calculator( 1, 3, Arthematic.Div);
+console.log(ans);
