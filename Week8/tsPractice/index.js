6e599bdc6f5eb2f2c36e1ab9903d8e698c309b0d
rambhardwajj <rambhardwaj101000@gmail.com>
2023-09-30 15:41:06 +0530

    Ts-Generics

---
+++ Week8/tsPractice/index.js
@@ -0,0 +1,20 @@
+// arrays in type script 
+function getFir(arr) {
+    return arr[0];
+}
+console.log(getFir([0, 2, 3]));
+console.log(getFir(["one", "two"]));
+// rather do this
+// Generics -> It refers to a programming concept which allows you to make classes methods or interfaces which can work with different types maintaning type safety 
+function getFirst(arr) {
+    return arr[0];
+}
+console.log(getFir([0, 2, 3]));
+console.log(getFir(["one", "two"]));
+// swap two numbers
+function swap(a, b) {
+    return [b, a];
+}
+console.log(swap(1, 3));
+console.log(swap("dfs", "sdfdsf"));
+console.log(swap("dfs", 1));
---
+++ Week8/tsPractice/index.ts
@@ -0,0 +1,34 @@
+
+// arrays in type script 
+
+type Input = (string | number)[];
+
+function getFir( arr: Input ): string | number{
+	return arr[0];
+}
+
+console.log(getFir([0,2,3]));
+console.log(getFir(["one", "two"])); 
+
+// rather do this
+// Generics -> It refers to a programming concept which allows you to make classes methods or interfaces which can work with different types maintaning type safety 
+
+function getFirst<T>(arr: T[]): T{
+	return arr[0];
+}
+console.log(getFir([0,2,3]));
+console.log(getFir(["one", "two"])); 
+
+// swap two numbers
+
+function swap<T,U>(a:T, b:U) : [U,T]{
+	
+	return  [b,a];
+}
+
+console.log( swap(1,3));
+console.log( swap("dfs","sdfdsf"));
+console.log( swap("dfs",1));
+
+
+
